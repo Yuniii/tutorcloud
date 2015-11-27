a54783e069a4d1cbe93044535cbbd2a80dc9659f
CodinCat <a55951234@gmail.com>
2015-11-28 01:32:12 +0800

    update homepage

--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,4 +1,22 @@
 <template>
-	<h1>Homepage</h1>
-	<a v-link="{path: '/room1/editor'}">Room1</a>
+	<div class="home-container uk-form">
+		<h1>TutorCloud</h1>
+		<label for="roomURL">輸入教室名稱：</label><input type="text" v-model="roomURL" id="roomURL" name="roomURL"> 
+		<a v-show="roomURL !== ''" v-link="{ path: '/' + roomURL + '/editor' }">點此進入{{ roomURL }}</a>
+	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			roomURL: ''
+		}
+	}
+}
+</script>
+
+<style lang="stylus">
+.home-container
+	padding 20px
+</style>
