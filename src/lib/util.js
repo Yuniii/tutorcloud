b3f75b14cbfd2e2adf8de90f6ed0abb756ac3b55
CodinCat <a55951234@gmail.com>
2015-12-01 00:44:39 +0800

    fill time

--- src/lib/util.js
+++ src/lib/util.js
@@ -32,7 +32,14 @@ export function decodeB64(str) {
 
 export function getNow() {
 	var now = new Date();
-	return '' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+	var h = now.getHours(),
+		m = now.getMinutes(),
+		s = now.getSeconds();
+	if (h < 10) h = '0' + h;
+	if (m < 10) m = '0' + m;
+	if (s < 10) s = '0' + s;
+
+	return '' + h + ':' + m + ':' + s;
 }
 
 export function isEmpty(str) {
