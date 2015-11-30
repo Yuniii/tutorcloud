ea107c1ab64b017df847c7065fa888e5abe42099
CodinCat <a55951234@gmail.com>
2015-12-01 00:56:41 +0800

    press enter to join room

--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="home-container uk-form">
 		<h1>TutorCloud</h1>
-		<label for="roomURL">輸入教室名稱：</label><input type="text" v-model="roomURL" id="roomURL" name="roomURL"> 
+		<label for="roomURL">輸入教室名稱：</label><input type="text" v-model="roomURL" id="roomURL" name="roomURL" @keyup="checkEnter"> 
 		<a v-show="roomURL !== ''" v-link="{ path: '/' + roomURL + '/editor' }">點此進入{{ roomURL }}</a>
 	</div>
 </template>
@@ -11,6 +11,13 @@ export default {
 	data() {
 		return {
 			roomURL: ''
+		}
+	},
+	methods: {
+		checkEnter(e) {
+			if (e.keyCode == 13) {
+				this.$route.router.go('/' + this.roomURL + '/editor');
+			}
 		}
 	}
 }
