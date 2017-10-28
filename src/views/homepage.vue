f627b920b352f81efb77e82e823f4ed9b6dc3f44
CodinCat <a55951234@gmail.com>
2017-10-28 16:20:54 +0800

    Update homepage. Add logo

--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,8 +1,14 @@
 <template>
 	<div class="home-container uk-form">
-		<h1>TutorCloud</h1>
-		<label for="roomURL">輸入教室名稱：</label><input type="text" v-model="roomURL" id="roomURL" name="roomURL" @keyup="checkEnter"> 
-		<a v-show="roomURL !== ''" v-link="{ path: '/' + roomURL + '/editor' }">點此進入{{ roomURL }}</a>
+		<div class="logo-container"><img src="/assets/images/logo.png" alt="TutorCloud"></div>
+		<div class="room-form">
+			<div class="room-input">
+				<label for="roomURL">請輸入討論室名稱：</label>
+				<input type="text" v-model="roomURL" id="roomURL" name="roomURL" @keyup="checkEnter">
+			</div>
+			<a v-show="roomURL !== ''" v-link="{ path: '/' + roomURL + '/editor' }">點此進入 {{ roomURL }}</a>
+			<a v-show="roomURL === ''" v-link="{ path: '/' }">&nbsp;</a>
+		</div>
 	</div>
 </template>
 
@@ -24,6 +30,23 @@ export default {
 </script>
 
 <style lang="stylus">
+@require '../styles/vars'
+
 .home-container
+	background main-color
+	text-align center
+	height 100%
 	padding 20px
+	.logo-container
+		max-width 300px
+		margin 40px auto
+	.room-form
+		background #FFF
+		width 600px
+		margin 0 auto
+		padding 80px
+		box-shadow 1px 1px 12px 0 rgba(0,0,0,0.36)
+	.room-input
+		margin-bottom 24px
+
 </style>
